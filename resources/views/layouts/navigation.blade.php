e35807428601a8ed4c7e10b09bdab054e805d202
Ivy Dekker <ivy_dk@hotmail.com>
2022-05-19 21:37:14 +0200

    posts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,16 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-{{--                    <a href="{{ route('dashboard') }}">--}}
+                    <a href="{{ route('redirects') }}">
                         <img src="img/id-logo.png" alt="id-logo" class="block h-10 w-auto fill-current ">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-{{--                        {{ __('Home') }}--}}
-{{--                    </x-nav-link>--}}
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                        {{ __('Posts') }}
+                    </x-nav-link>
                 </div>
             </div>
 
