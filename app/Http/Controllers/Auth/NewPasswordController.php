9b6134fefa924788acd54607ecbd92847b4d7cf2
Ivy Dekker <ivy_dk@hotmail.com>
2022-05-20 21:29:27 +0200

    strong password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -36,7 +36,17 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => ['required'],
             'email' => ['required', 'email'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            // Strong password
+            'password' => [
+                'required',
+                'confirmed',
+                Rules\Password::min(8)
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised()
+                ]
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we
