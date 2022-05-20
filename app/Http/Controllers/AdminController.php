a23c62a8b5ace8fe9a70fe35b899bad69ef28437
Ivy Dekker <ivy_dk@hotmail.com>
2022-05-20 19:44:10 +0200

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,6 +16,6 @@ class AdminController extends Controller
      */
     public function index(Request $request)
     {
-        return view('dashboard');
+        return view('\dashboard');
     }
 }
