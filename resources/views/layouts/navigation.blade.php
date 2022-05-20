fa658b213ea19ce4172bc3c0872262ba86a041ed
Ivy Dekker <ivy_dk@hotmail.com>
2022-05-20 18:00:36 +0200

    crud posts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,12 +11,39 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                        {{ __('Posts') }}
-                    </x-nav-link>
+                <!-- Posts dropdown -->
+                <div class="hidden sm:flex sm:items-center sm:ml-6 pt-1">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>Posts</div>
+
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+                        <!-- Dropdown items -->
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                                {{ __('All posts') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('posts.index')" :active="request()->routeIs('posts.index')" >
+                                {{ __('My posts') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('posts.create')" :active="request()->routeIs('posts.create')">
+                                {{ __('Create post') }}
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
                 </div>
+
+
+
             </div>
+
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -32,6 +59,8 @@
                             </div>
                         </button>
                     </x-slot>
+
+
 
                     <x-slot name="content">
                         <!-- Authentication -->
