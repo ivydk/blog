8ed3ff3c04e6a17c36580219ed38120abe1f791a
Ivy Dekker <ivy_dk@hotmail.com>
2022-05-21 23:30:48 +0200

    small things

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,11 +3,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <p class="text-xl">Hello {{ auth()->user()->name }}!</p>
+                    <p> You are logged in as an
                     @if(auth()->user()->role === 'admin')
-                    Admin!
+                    admin.
                     @else
-                    User!
+                    user.
                     @endif
+                    </p>
                 </div>
             </div>
         </div>
