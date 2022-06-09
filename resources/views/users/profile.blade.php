ddc9f8094c30a549a6f85b6af5bf404e2df1de5d
Ivy Dekker <ivy_dk@hotmail.com>
2022-06-09 14:47:20 +0200

    user info on blade

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -3,14 +3,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <img id="avatar" src="{{ $user->avatar }}" alt="avatar">
-                    {{ $user->name }}
-                    <form enctype="multipart/form-data" action="/profile" method="POST">
+
+                    <div class="flex flex-row">
+                        <div class="pr-8"><img id="avatar" src="{{ $user->avatar }}" alt="avatar"></div>
+                        <div>
+                            <table>
+                                <tr>
+                                    <td class="profile-table-first-row"> Name:</td>
+                                    <td> {{ $user->name }} </td>
+                                </tr>
+                                <tr>
+                                    <td class="profile-table-first-row"> E-mail:</td>
+                                    <td> {{ $user->email }} </td>
+                                </tr>
+                                <tr>
+                                    <td class="profile-table-first-row"> Role:</td>
+                                    <td> {{ $user->role }} </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+
+
+                    <form enctype="multipart/form-data" action="{{ route('profile') }}" method="POST">
                         <label>Update Profile Image</label>
                         <input type="file" name="avatar">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="submit" class="pull-right btn btn-sm btn-primary">
                     </form>
+
                 </div>
             </div>
         </div>
