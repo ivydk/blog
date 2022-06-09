b3b859213b12c4e7ca900dc5684b94e9775476bd
Ivy Dekker <ivy_dk@hotmail.com>
2022-06-09 15:01:45 +0200

    Update profile.blade.php

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -3,7 +3,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
                     <div class="flex flex-row">
                         <div class="pr-8"><img id="avatar" src="{{ $user->avatar }}" alt="avatar"></div>
                         <div>
@@ -31,7 +30,6 @@
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="submit" class="pull-right btn btn-sm btn-primary">
                     </form>
-
                 </div>
             </div>
         </div>
