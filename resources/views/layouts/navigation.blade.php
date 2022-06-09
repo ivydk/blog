ebf01e03d49fea6160d8d2f243678fb0baa0ce7f
Ivy Dekker <ivy_dk@hotmail.com>
2022-06-09 14:34:10 +0200

    dropdown profile page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -63,10 +63,13 @@
 
 
                     <x-slot name="content">
+                        <!-- Profile page -->
+                        <x-dropdown-link :href="route('profile')" :active="request()->routeIs('profile')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
@@ -74,6 +77,7 @@
                             </x-dropdown-link>
                         </form>
                     </x-slot>
+
                 </x-dropdown>
             </div>
 
@@ -92,7 +96,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-{{--            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
+{{--            <x-responsive-nav-link :href="route('user_dashboard')" :active="request()->routeIs('dashboard')">--}}
 {{--                {{ __('Dashboard') }}--}}
 {{--            </x-responsive-nav-link>--}}
         </div>
