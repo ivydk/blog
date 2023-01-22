d764bc601bfad81ae3a273cbef67386f3bbc11ba
Ivy Dekker <ivy_dk@hotmail.com>
2023-01-22 12:12:08 +0100

    changed welcome message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <p class="text-xl">Hello {{ auth()->user()->name }}!</p>
-                    <p> You are logged in as an
+                    <p> Welcome! You are logged in as an
                     @if(auth()->user()->role === 'admin')
                     admin.
                     @else
